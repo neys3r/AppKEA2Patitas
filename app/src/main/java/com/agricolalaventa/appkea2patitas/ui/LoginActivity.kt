33a0efa2fa06d334840cb40f7e06bd2535b3e9ea
nhuaman <neyser@outlook.com>
2020-11-25 21:00:42 -0500

    Commit creación Proyecto Patitas

---
+++ app/src/main/java/com/agricolalaventa/appkea2patitas/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.agricolalaventa.appkea2patitas.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.agricolalaventa.appkea2patitas.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
